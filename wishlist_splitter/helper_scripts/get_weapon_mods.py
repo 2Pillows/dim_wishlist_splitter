dcc9aab3bfa0600141e9382c9acaaf75293dc372
2Pillows <25855654+2Pillows@users.noreply.github.com>
2024-06-12 17:51:53 -0400

    add comments and consolidate getting perk hashes

--- wishlist_splitter/helper_scripts/get_weapon_mods.py
+++ wishlist_splitter/helper_scripts/get_weapon_mods.py
@@ -2,16 +2,25 @@
 
 import json
 
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from main import Keys
+
 
 #############################
 # Gets array from file_path #
 #############################
-def get_weapon_mods(file_path: str) -> set:
+def get_weapon_mods(keys: "Keys") -> set:
     # Empty set to hold frame mod hashes
-    weapon_mods_set = set()
+    origin_traits = set()
+    frame_mods = set()
 
     # Open and read the JSON file
-    with open(file_path, "r", encoding="utf-8") as file:
-        weapon_mods_set = set(json.load(file))
+    with open(keys.ORIGIN_TRAITS_PATH, "r", encoding="utf-8") as origin_traits_file:
+        origin_traits = set(json.load(origin_traits_file))
 
-    return weapon_mods_set
+    with open(keys.FRAME_MODS_PATH, "r", encoding="utf-8") as frame_mods_file:
+        frame_mods = set(json.load(frame_mods_file))
+
+    return origin_traits, frame_mods
--- wishlist_splitter/main.py
+++ wishlist_splitter/main.py
@@ -1,52 +1,39 @@
 # main.py
+
 from collections import Counter
 from dataclasses import dataclass
-
 from typing import List, Dict, Set
 
-# import time
-
-# start_time = time.time()
-
-###########################################################
-# Called from Github Workflow to start updating wishlist #
-# Collects wishlist config and voltron data              #
-# Then sorts and writes voltron data to wishlists        #
-###########################################################
-
-# Import config file
+# Import helper functions
 from data.wishlist_configs import get_wishlist_config
-
-# Import helper function to grab origin trait hashes
 from helper_scripts.get_weapon_mods import get_weapon_mods
-
-# Import helper functions for getting voltron data
 from helper_scripts.extract_voltron_data import extract_voltron_data
-
 from helper_scripts.write_to_wishlists import write_to_wishlists
+
+# Timer to test script performance
+# import time
+# start_time = time.time()
 
 
 # Class to store constants that reference keys or values
 @dataclass
 class Keys:
-    #########
-    # Paths #
-    #########
+
+    # Main text file with all weapon rolls
     VOLTRON_PATH = "./wishlist_splitter/data/dim-wish-list-sources/voltron.txt"
     # VOLTRON_PATH = "./wishlist_splitter/data/test.txt"
+
+    # Origin traits and frame mods from Destiny Data Explorer
     ORIGIN_TRAITS_PATH = "./wishlist_splitter/data/weapon_mods/origin_traits.txt"
     FRAME_MODS_PATH = "./wishlist_splitter/data/weapon_mods/frame_mods.txt"
-    # Path to all wishlist paths for website
-    WISHLIST_NAMES_PATH = "./docs/data/wishlist_names.txt"
-    # Directory to hold wishlists
+
+    WISHLIST_NAMES_PATH = "./docs/data/wishlist_names.txt"  # Wishlists for website ref
+
     WISHLIST_DIR = "./wishlists/"
 
-    ############################
-    # Keys for wishlist config #
-    ############################
+    # Keys to reference wishlist config and voltron data
     WISHLIST_CONFIGS_KEY = "wishlist_configs"
     PATH_KEY = "path"
-
     CREDIT_KEY = "credits"
     CREDIT_TAG = "credits"
     AUTHOR_KEY = "author"
@@ -56,72 +43,56 @@ class Keys:
     PERKS_KEY = "perks"
     DUPES_KEY = "dupes"
 
-    ######################################
-    # Keys for storing data from voltron #
-    ######################################
-    # Core perks are used for accurate counts of perks
-    # Key for storing list of base perks (only 1st, 2nd, 3rd, and 4th columns)
-    CORE_PERKS_KEY = "core_perks"
-    # Key for storing trimmed perks (3rd and 4th columns with any extra perks)
-    TRIMMED_PERKS_KEY = "trimmed_perks"
-    # Key for storing core perks that have been filtered (only 3rd and 4th column)
-    CORE_TRIMMED_PERKS_KEY = "core_trimmed_perks"
+    TRIMMED_PERKS_KEY = "trimmed_perks"  # Only 3rd, 4th, and origin traits
+    # Core perks are used to ensure accurate count for perks when filtering dupes
+    CORE_PERKS_KEY = "core_perks"  # No origin traits
+    CORE_TRIMMED_PERKS_KEY = "core_trimmed_perks"  # Only 3rd and 4th column
 
-    # Minimum number of rolls to for dupe rolls
-    MIN_ROLL_COUNT = 2
+    MIN_ROLL_COUNT = 2  # Minimum number of rolls to for dupe rolls
 
-    # Numebr of rolls held for wishlist before writing
-    BATCH_SIZE = 1000
+    BATCH_SIZE = 1000  # Numebr of rolls held for wishlist before writing
 
-    # Keys to be added
-    # Voltron text
-    VOLTRON_DATA: List[Dict[str, object]] = None
-    # Dictionary storing wishlist information
-    WISHLIST_CONFIGS: List[Dict[str, object]] = None
-    # List of origin trait hashes
-    ORIGIN_TRAITS: Set[str] = None
-    # List of frame mod hashes, 3rd and 4th column
-    FRAME_MODS: Set[str] = None
-    # List of tag values for all, include, and exclude
-    INC_TAGS: Set[str] = None
-    EXC_TAGS: Set[str] = None
-    # List of author names
-    AUTHOR_NAMES: Set[str] = None
+    # Placeholder objects to hold values from voltron and wishlists
+    VOLTRON_DATA: List[Dict[str, object]] = None  # Voltron rolls sorted and tagged
+    WISHLIST_CONFIGS: List[Dict[str, object]] = None  # Wishlist preferences
+    ORIGIN_TRAITS: Set[str] = None  # List of origin trait hashes
+    FRAME_MODS: Set[str] = None  # List of frame mod hashes, 3rd and 4th column
+    INC_TAGS: Set[str] = None  # All tags that wishlist want to include
+    EXC_TAGS: Set[str] = None  # All tags that wishlists want to exclude
+    AUTHOR_NAMES: Set[str] = None  # All author names in wishlists configs
 
     # Counters for perks and weapons
-    CORE_COUNTER = Counter()
-    TRIMMED_COUNTER = Counter()
-    WEAPON_COUNTER = Counter()
+    CORE_COUNTER = Counter()  # Counter of core_perks
+    TRIMMED_COUNTER = Counter()  # Counter for trimmed perks
+    WEAPON_COUNTER = Counter()  # Counter for appearences of weapons
 
 
 def main():
+    # Create object for Keys
     keys = Keys()
 
-    # Pass helper keys to get wishlist configs with matching keys
-    # Wishlists are exported from this function
+    # Get wishlist data and save to keys
+    # Wishlist names are exported as well
     config_results = get_wishlist_config(keys)
     keys.WISHLIST_CONFIGS = config_results[keys.WISHLIST_CONFIGS_KEY]
     keys.AUTHOR_NAMES = config_results[keys.AUTHOR_KEY]
     keys.INC_TAGS = config_results[keys.INC_TAGS_KEY]
     keys.EXC_TAGS = config_results[keys.EXC_TAGS_KEY]
 
-    # Collect origin trait hashes
-    keys.ORIGIN_TRAITS = get_weapon_mods(keys.ORIGIN_TRAITS_PATH)
+    # Collect origin trait and frame mod hashes to keys
+    keys.ORIGIN_TRAITS, keys.FRAME_MODS = get_weapon_mods(keys)
 
-    # Collect frame mod hashes
-    keys.FRAME_MODS = get_weapon_mods(keys.FRAME_MODS_PATH)
-
-    # Collect data from voltron
+    # Collect data from voltron to keys
     keys.VOLTRON_DATA = extract_voltron_data(keys)
 
-    # Write voltron data to config files
+    # Write voltron data to wishlist files
     write_to_wishlists(keys)
 
 
 if __name__ == "__main__":
     main()
 
+    # Timer to see main script performance
     # end_time = time.time()
-
     # runtime = end_time - start_time
     # print(f"Runtime: {runtime} seconds")
