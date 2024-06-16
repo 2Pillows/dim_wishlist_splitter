6556b5442098ac0628b7b669e91f37f91f48d38c
2Pillows <25855654+2Pillows@users.noreply.github.com>
2024-06-16 01:52:00 -0400

    fix keys import

--- wishlist_splitter/data/wishlist_configs.py
+++ wishlist_splitter/data/wishlist_configs.py
@@ -3,9 +3,8 @@
 import json
 from typing import TYPE_CHECKING
 
-# Load Keys class without importing to avoid cyclic import
-if TYPE_CHECKING:
-    from main import Keys
+# Import keys
+from helper_scripts.keys import Keys
 
 
 def get_wishlist_config(keys: "Keys"):
